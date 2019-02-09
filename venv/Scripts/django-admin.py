2b76275b59d865e99253553cf067c9f86a68d959
Dexcrash <ca.sanchez38@uniandes.edu.co>
2019-02-09 17:36:54 -0500

    prueba

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\cass_\Proyectos\Clase\GRUPO-3-PROCESOS_AGILES_DE_DESARROLLO\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
