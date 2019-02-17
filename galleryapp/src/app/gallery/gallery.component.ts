fc794c36e13847bd00abf27a4f2da1b7598263e7
Dexcrash <ca.sanchez38@uniandes.edu.co>
2019-02-17 16:03:25 -0500

    Arreglos en los videos y video tag

--- galleryapp/src/app/gallery/gallery.component.ts
+++ galleryapp/src/app/gallery/gallery.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Multimedia } from '../services/multimedia/multimedia';
 import { MultimediaService } from '../services/multimedia/multimedia.service';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-gallery',
@@ -12,14 +13,22 @@ export class GalleryComponent implements OnInit {
   multimedias: Multimedia[];
   selectedImage: Multimedia;
 
-  constructor(private imageService: MultimediaService) { }
+  constructor(private sanitizer:DomSanitizer, private imageService: MultimediaService) { }
 
   ngOnInit() {
     this.getImages();
+    this.transformUrls();
   }
 
   getImages(): void {
     this.imageService.getMultimedias()
         .subscribe(multimedias => this.multimedias = multimedias);
+  }
+
+
+  transformUrls(): void{
+    for (let mul of this.multimedias){
+      mul.url = "" + this.sanitizer.bypassSecurityTrustResourceUrl(mul.url.valueOf());
+    }
   }
 }
